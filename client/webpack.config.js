8d7920664d17cd952cc99ba1dae86e84174d005d
Hoseop Jeong <hosap4ik@gmail.com>
2022-07-23 14:04:15 +0200

    filter boolean

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       new webpack.DefinePlugin({
         "process.env":JSON.stringify(process.env)
       })
-    ],
+    ].filter(Boolean),
     resolve: {
       modules: [__dirname, "src", "node_modules"],
       extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
