73de1174f9b260aabd4b17d0f8ea203828597878
Hoseop Jeong <hosap4ik@gmail.com>
2022-08-11 00:07:59 +0200

    removed possibly undefined issue

--- client/src/Item.tsx
+++ client/src/Item.tsx
@@ -3,10 +3,17 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { removeSearch,viewItem } from './redux/action_functions';
+interface ItemDetail{
+    name:string,
+    opening_hours:string[],
+    address:string,
+    reviews:string[],
+    photoRef:string
+}
 const apiKey=process.env.apiKey;
 const Item= ()=>{
     const dispatch = useDispatch();
-    const [itemData,setItemDataState]=useState<Object>({});
+    const [itemData,setItemDataState]=useState<ItemDetail>();
     useEffect(()=>{
         dispatch(removeSearch());
         dispatch(viewItem());
@@ -36,7 +43,7 @@ const Item= ()=>{
             {Object.keys(itemData).length>0
             ?
             <div>
-                <h1>{itemData.name}</h1>
+                <h1>{itemData?.name}</h1>
                 <div id="DetailedInformationContainer">
                     <figure className="inline-block placeImage">
                         <img src={"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference="+itemData.photoRef+"&key="+apiKey}/>
@@ -46,13 +53,13 @@ const Item= ()=>{
                             <ul className="table-cell">
                                 <p>Opening hours:</p>
                                 {
-                                    itemData.opening_hours.map((item,index)=>{
+                                    itemData?.opening_hours.map((item,index)=>{
                                         return <li>{item}</li>
                                     })
                                 }
                             </ul>
                         </div>
-                        <p>Adress: {itemData.address}</p>
+                        <p>Adress: {itemData?.address}</p>
                     </div>
                 </div>
                 <ul id="Comments">
@@ -64,10 +71,10 @@ const Item= ()=>{
                             <li key={"item-"+index*index}>
                                 <div className="commentDiv">
                                     <div>
-                                        <div className="w-1/2 text-center">{itemData.reviews[index]['author_name']}</div>
-                                        <div className="w-1/2 text-center">{itemData.reviews[index]['rating']}/5</div>
+                                        <div className="w-1/2 text-center">{itemData?.reviews[index]['author_name']}</div>
+                                        <div className="w-1/2 text-center">{itemData?.reviews[index]['rating']}/5</div>
                                     </div>
-                                    <div className="commentText">{itemData.reviews[index]['text']}</div>
+                                    <div className="commentText">{itemData?.reviews[index]['text']}</div>
                                 </div>
                             </li>
                             )
