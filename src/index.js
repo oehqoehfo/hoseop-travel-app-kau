568d95cc8e8f1d86a4a26442800ee2f06056c07b
Hoseop Jeong <hosap4ik@gmail.com>
2022-08-13 02:02:43 +0200

    router

--- src/index.js
+++ src/index.js
@@ -85,8 +85,8 @@ app.listen(process.env.PORT||port, () => {
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get("*", (req, res) => {
+  router.use(express.static(path.join(__dirname, '../client/dist')));
+  router.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,  "../client/dist", "index.html"));
   });
 }
